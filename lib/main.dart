8dca42308455fb9887ae56f64690b91547eaa066
Utsav <udhungana@himalayantechies.com>
2023-01-21 23:00:47 +0545

    Refactored to notification_services

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,11 @@
-import 'dart:convert';
 import 'dart:developer';
-import 'dart:ffi';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:http/http.dart';
-import 'package:notification_app/getFcm.dart';
 import 'package:notification_app/second_screen.dart';
-import 'package:http/http.dart';
+import 'package:notification_app/services/notification_servicers.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,68 +14,6 @@ Future<void> main() async {
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBankgroundHandler);
 
   runApp(const MyApp());
-}
-
-Future _showNotification(RemoteMessage message) async {
-  const AndroidNotificationChannel channel = AndroidNotificationChannel(
-    'high_importance_channel', //id,
-    'High Importance Notifications', //title
-    description:
-        'This channel is used for important notifications.', //description
-    importance: Importance.max,
-    playSound: true,
-  );
-
-  await flutterLocalNotificationsPlugin
-      .resolvePlatformSpecificImplementation<
-          AndroidFlutterLocalNotificationsPlugin>()
-      ?.createNotificationChannel(channel);
-
-  log(message.toString());
-  RemoteNotification? notification = message.notification;
-  AndroidNotification? android = message.notification?.android;
-  Map<String, dynamic> dataValue = message.data;
-  BigPictureStyleInformation? bigPictureStyleInformation;
-  AndroidBitmap<Object>? largeIcon;
-  if (notification != null) {
-    String screen = dataValue['screen'].toString();
-    log('Data: ${message.data.toString()}');
-    log('Image URL: ${message.notification?.android?.imageUrl.toString()}');
-    if (android != null && android.imageUrl != null) {
-      Response response = await get(
-        Uri.parse(message.notification!.android!.imageUrl.toString()),
-      );
-      bigPictureStyleInformation = BigPictureStyleInformation(
-        ByteArrayAndroidBitmap.fromBase64String(
-          base64Encode(response.bodyBytes),
-        ),
-      );
-      largeIcon = ByteArrayAndroidBitmap.fromBase64String(
-        base64Encode(response.bodyBytes),
-      );
-    }
-
-    flutterLocalNotificationsPlugin.show(
-      notification.hashCode,
-      notification.title,
-      notification.body,
-      NotificationDetails(
-        android: AndroidNotificationDetails(
-          channel.id,
-          channel.name,
-          channelDescription: channel.description,
-          color: Colors.blue,
-          playSound: true,
-          icon: '@mipmap/marvel_notification_icon',
-          largeIcon: largeIcon,
-          styleInformation: bigPictureStyleInformation,
-        ),
-      ),
-      payload: screen,
-    );
-  } else {
-    log('IN Else');
-  }
 }
 
 final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
@@ -119,140 +53,25 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
+  late final NotificationService notificationService;
 
   @override
   void initState() {
     super.initState();
-    getNotificationPermission();
-    getToken();
-
-    handleMessageOnBackground();
-
-    var initializationSettingsAndroid =
-        const AndroidInitializationSettings('@mipmap/marvel_notification_icon');
-
-    final InitializationSettings initializationSettings =
-        InitializationSettings(
-      android: initializationSettingsAndroid,
-    );
-
-    flutterLocalNotificationsPlugin.initialize(
-      initializationSettings,
-      onDidReceiveNotificationResponse: onNotificationTap,
-    );
-
-    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-      log('listening to message');
-      _showNotification(message);
-    });
-
-    //when app is in background but not terminated.
-    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
-      log('on Message Opened App');
-      RemoteNotification? notification = message.notification;
-
-      if (notification != null) {
-        log('opened onmessageopen: ${message.data}');
-        if (message.data['screen'] == 'secondScreen') {
-          Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: (context) => const SecondScreen(),
-            ),
-          );
-        } else {
-          log('in onMessageOpenedApp else section');
-        }
-      }
-    });
+    notificationService = NotificationService();
+    listenToNotificationStream();
+    notificationService.initializePlatformNotifications();
   }
 
-  void handleMessageOnBackground() {
-    FirebaseMessaging.instance.getInitialMessage().then(
-      (remoteMessage) {
-        log('on handleMessageOnBackground');
-        if (remoteMessage != null) {
-          if (remoteMessage.data['screen'] == 'secondScreen') {
-            Future.delayed(const Duration(milliseconds: 1000), () async {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => const SecondScreen(),
-                ),
-              );
-            });
-          } else {
-            log('in onMessageOpenedApp else section');
-          }
-        }
-      },
-    );
-  }
-
-  void getNotificationPermission() async {
-    NotificationSettings settings = await _firebaseMessaging.requestPermission(
-      alert: true,
-      announcement: false,
-      badge: true,
-      carPlay: false,
-      criticalAlert: false,
-      provisional: false,
-      sound: true,
-    );
-    log('User granted permission: ${settings.authorizationStatus}');
-
-    await FirebaseMessaging.instance
-        .setForegroundNotificationPresentationOptions(
-      alert: true,
-      badge: true,
-      sound: true,
-    );
-  }
-
-  getToken() async {
-    await FirebaseMessaging.instance.getToken();
-  }
-
-  Future<dynamic> onNotificationTap(NotificationResponse response) async {
-    log('On Notification Tap: ${response.payload}');
-    if (response.payload == 'secondScreen') {
-      Navigator.of(context).push(
-        MaterialPageRoute(
-          builder: (context) => const SecondScreen(),
-        ),
-      );
-    } else {
-      log('in On tap else');
-    }
-  }
-
-  @pragma('vm:entry-point')
-  Future<dynamic> onBackGroundNotificationTap(
-      NotificationResponse response) async {
-    log('On Notification Tap: ${response.payload}');
-    if (response.payload == 'secondScreen') {
-      Navigator.of(context).push(
-        MaterialPageRoute(
-          builder: (context) => const SecondScreen(),
-        ),
-      );
-    } else {
-      log('in On tap else');
-    }
-  }
-
-  void showNotification() async {
-    setState(() {
-      _counter++;
-    });
-    _showNotification(
-      RemoteMessage(
-        notification: RemoteNotification(
-          title: 'Texting $_counter',
-          body: 'Notifying the user.',
-        ),
-      ),
-    );
-  }
+  void listenToNotificationStream() =>
+      notificationService.behaviorSubject.listen((payload) {
+        log('listenToNotificationStream function');
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => const SecondScreen(),
+          ),
+        );
+      });
 
   @override
   Widget build(BuildContext context) {
@@ -275,7 +94,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: showNotification,
+        onPressed: () {},
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
--- lib/services/notification_servicers.dart
+++ lib/services/notification_servicers.dart
@@ -1,22 +1,27 @@
+import 'dart:convert';
 import 'dart:developer';
-import 'dart:html';
-import 'dart:io';
-import 'dart:ui';
 
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:http/http.dart';
 
 import 'package:rxdart/rxdart.dart';
-
-import '../second_screen.dart';
 
 class NotificationService {
   NotificationService();
 
   final _flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
   final BehaviorSubject<String> behaviorSubject = BehaviorSubject();
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
 
   Future<void> initializePlatformNotifications() async {
+    log('hereeeeeeeeeee');
+    getNotificationPermission();
+    getToken();
+
+    handleMessageOnBackground();
+
     const AndroidInitializationSettings initializationSettingsAndroid =
         AndroidInitializationSettings('@mipmap/marvel_notification_icon');
 
@@ -39,6 +44,31 @@ class NotificationService {
       onDidReceiveNotificationResponse: onNotificationTap,
       // onDidReceiveBackgroundNotificationResponse: onBackGroundNotificationTap,
     );
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      log('listening to message');
+      _showNotification(message);
+    });
+
+    //when app is in background but not terminated.
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      log('on Message Opened App');
+      RemoteNotification? notification = message.notification;
+
+      if (notification != null) {
+        log('opened onmessageopen: ${message.data}');
+        if (message.data['screen'] == 'secondScreen') {
+          behaviorSubject.add(message.data['screen']);
+          // Navigator.of(context).push(
+          //   MaterialPageRoute(
+          //     builder: (context) => const SecondScreen(),
+          //   ),
+          // );
+        } else {
+          log('in onMessageOpenedApp else section');
+        }
+      }
+    });
   }
 
   Future<dynamic> onNotificationTap(NotificationResponse response) async {
@@ -65,27 +95,126 @@ class NotificationService {
     print('id $id');
   }
 
-//   Future<NotificationDetails> _notificationDetails() async {
-//     final bigPicture = await DownloadUtil.downloadAndSaveFile(
-//         "https://images.unsplash.com/photo-1624948465027-6f9b51067557?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-//         "drinkwater");
+  void getNotificationPermission() async {
+    NotificationSettings settings = await _firebaseMessaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+    log('User granted permission: ${settings.authorizationStatus}');
 
-//     AndroidNotificationDetails androidPlatformChannelSpecifics =
-//         AndroidNotificationDetails(
-//       'channel id',
-//       'channel name',
-//       groupKey: 'com.example.flutter_push_notifications',
-//       channelDescription: 'channel description',
-//       importance: Importance.max,
-//       priority: Priority.max,
-//       playSound: true,
-//       ticker: 'ticker',
-//       largeIcon: const DrawableResourceAndroidBitmap('justwater'),
-//       styleInformation: BigPictureStyleInformation(
-//         FilePathAndroidBitmap(bigPicture),
-//         hideExpandedLargeIcon: false,
-//       ),
-//       color: const Color(0xff2196f3),
-//     );
-//   }
+    await FirebaseMessaging.instance
+        .setForegroundNotificationPresentationOptions(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+  }
+
+  getToken() async {
+    String? token = await FirebaseMessaging.instance.getToken();
+    log(token!);
+    return token;
+  }
+
+  Future _showNotification(RemoteMessage message) async {
+    AndroidNotificationChannel androidPlatformChannelSpecifics =
+        const AndroidNotificationChannel(
+      'channel id',
+      'channel name',
+      description: 'channel description',
+      importance: Importance.max,
+      playSound: true,
+    );
+
+    DarwinNotificationDetails iosNotificationDetails =
+        const DarwinNotificationDetails(
+      threadIdentifier: "thread1",
+      //   attachments: <DarwinNotificationAttachment>[
+      // IOSNotificationAttachment(bigPicture)
+      // ]
+    );
+
+    // final details = await _flutterLocalNotificationsPlugin.getNotificationAppLaunchDetails();
+    // if (details != null && details.didNotificationLaunchApp) {
+    //   behaviorSubject.add(details.payload!);
+    // }
+
+    await _flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            AndroidFlutterLocalNotificationsPlugin>()
+        ?.createNotificationChannel(androidPlatformChannelSpecifics);
+
+    log(message.toString());
+    RemoteNotification? notification = message.notification;
+    AndroidNotification? android = message.notification?.android;
+    Map<String, dynamic> dataValue = message.data;
+    BigPictureStyleInformation? bigPictureStyleInformation;
+    AndroidBitmap<Object>? largeIcon;
+    if (notification != null) {
+      String screen = dataValue['screen'].toString();
+      log('Data: ${message.data.toString()}');
+      log('Image URL: ${message.notification?.android?.imageUrl.toString()}');
+      if (android != null && android.imageUrl != null) {
+        Response response = await get(
+          Uri.parse(message.notification!.android!.imageUrl.toString()),
+        );
+        bigPictureStyleInformation = BigPictureStyleInformation(
+          ByteArrayAndroidBitmap.fromBase64String(
+            base64Encode(response.bodyBytes),
+          ),
+        );
+        largeIcon = ByteArrayAndroidBitmap.fromBase64String(
+          base64Encode(response.bodyBytes),
+        );
+      }
+
+      _flutterLocalNotificationsPlugin.show(
+        notification.hashCode,
+        notification.title,
+        notification.body,
+        NotificationDetails(
+          android: AndroidNotificationDetails(
+            androidPlatformChannelSpecifics.id,
+            androidPlatformChannelSpecifics.name,
+            channelDescription: androidPlatformChannelSpecifics.description,
+            color: Colors.blue,
+            playSound: true,
+            icon: '@mipmap/marvel_notification_icon',
+            largeIcon: largeIcon,
+            styleInformation: bigPictureStyleInformation,
+          ),
+        ),
+        payload: screen,
+      );
+    } else {
+      log('IN Else');
+    }
+  }
+
+  void handleMessageOnBackground() {
+    FirebaseMessaging.instance.getInitialMessage().then(
+      (remoteMessage) {
+        log('on handleMessageOnBackground');
+        if (remoteMessage != null) {
+          if (remoteMessage.data['screen'] == 'secondScreen') {
+            Future.delayed(const Duration(milliseconds: 1000), () async {
+              behaviorSubject.add(remoteMessage.data['screen']);
+              // Navigator.of(context).push(
+              //   MaterialPageRoute(
+              //     builder: (context) => const SecondScreen(),
+              //   ),
+              // );
+            });
+          } else {
+            log('in onMessageOpenedApp else section');
+          }
+        }
+      },
+    );
+  }
 }
